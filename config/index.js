6ece1459b5c4f5a16f8c41fca22b760db959f8c9
Mate Bodor <bodormate@gmail.com>
2021-04-28 00:04:21 +0200

    add url to locla api

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 const isDev = process.env.NODE_ENV !== 'production';
-export const apiServerUrl = isDev ? 'http://localhost:3000' : 'https://yourwebsite.com';
+export const apiServerUrl = isDev ? 'http://localhost:3000' : 'https://weekend-offer-browser.vercel.app';
 export const externalUrl = process.env.MAIN_URL;
 export const pageSize = 5;
 export const maximumResult = 50;
